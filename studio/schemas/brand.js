3006b5e86f235d3684396e7bfb9e1f276aab35e9
1wku <beo1692@gmail.com>
2022-10-04 23:19:48 +0700

    create new server

---
+++ studio/schemas/brand.js
@@ -0,0 +1,29 @@
+export default {
+  name: "brand",
+  title: "Brand",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 96,
+      },
+    },
+    {
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
--- studio/schemas/category.js
+++ studio/schemas/category.js
@@ -9,29 +9,9 @@ export default {
       type: 'string',
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'title',
-        maxLength: 96,
-      },
-    },
-    {
       name: 'description',
       title: 'Description',
       type: 'text',
-    },
-    {
-      name: 'parents',
-      title: 'Parent categories',
-      type: 'array',
-      of: [
-        {
-          type: 'reference',
-          to: [{type: 'category'}],
-        },
-      ],
     },
   ],
 }
